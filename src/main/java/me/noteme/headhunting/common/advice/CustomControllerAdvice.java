e4a1002b89a2948ecb54959b492750ac35a5446f
김용수 <akys159357@naver.com>
2024-10-21 15:54:40 +0900

    [#1]feat: 전역 ExceptionHandler 생성

--- src/main/java/me/noteme/headhunting/common/advice/CustomControllerAdvice.java
+++ src/main/java/me/noteme/headhunting/common/advice/CustomControllerAdvice.java
@@ -1,2 +1,49 @@
-package me.noteme.headhunting.common.advice;public class CustomControllerAdvice {
+package me.noteme.headhunting.common.advice;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.noteme.headhunting.common.exception.CustomException;
+import me.noteme.headhunting.common.exception.ErrorCode;
+import me.noteme.headhunting.common.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class CustomControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception exception) {
+        log.error("Exception : {}", exception.getMessage());
+        return ErrorResponse.of(exception);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse NoResourceFoundException(NoResourceFoundException exception){
+        log.error("잘못된 endPoint : ", exception);
+        return ErrorResponse.of(new CustomException(ErrorCode.RESOURCE_NOT_FOUND));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class, MissingServletRequestParameterException.class, HttpMessageNotReadableException.class})
+    public ErrorResponse handleBadRequestException(Exception exception) {
+        log.error("잘못된 요청 ", exception);
+        return ErrorResponse.of(new CustomException(ErrorCode.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ErrorResponse handleCustomException(CustomException exception) {
+        log.error("CustomException: {}", exception.getMessage());
+        return ErrorResponse.of(exception);
+    }
 }
