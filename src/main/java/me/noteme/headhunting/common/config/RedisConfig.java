4cce6e9c883afb2c358194982f65fd5e1e0015fa
김용수 <akys159357@naver.com>
2024-10-25 16:06:22 +0900

    [#26]feat: 레디스 환경설정

---
+++ src/main/java/me/noteme/headhunting/common/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package me.noteme.headhunting.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    private String host;
+
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        StringRedisSerializer serializer = new StringRedisSerializer();
+        redisTemplate.setDefaultSerializer(serializer);
+
+        return redisTemplate;
+    }
+}
