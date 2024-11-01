c77f19bb137e13e0c6163ae025b6685b029f9339
김용수 <akys159357@naver.com>
2024-11-01 22:45:48 +0900

    [#64]feat: 알람 처리 구현

--- src/main/java/me/noteme/headhunting/common/advice/CustomControllerAdvice.java
+++ src/main/java/me/noteme/headhunting/common/advice/CustomControllerAdvice.java
@@ -1,8 +1,10 @@
 package me.noteme.headhunting.common.advice;
 
 import io.sentry.Sentry;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.noteme.headhunting.common.alarm.service.MattermostNotificationService;
 import me.noteme.headhunting.common.exception.CustomException;
 import me.noteme.headhunting.common.exception.ErrorCode;
 import me.noteme.headhunting.common.response.ErrorResponse;
@@ -16,38 +18,62 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.util.Enumeration;
+
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class CustomControllerAdvice {
+    private final MattermostNotificationService notificationService;
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleException(Exception exception) {
-        log.error("Exception : ", exception);
+    public ErrorResponse handleException(Exception exception, HttpServletRequest request) {
         Sentry.captureException(exception);
+        sendNotification(exception, request);
+
         return ErrorResponse.of(new CustomException(ErrorCode.SERVER_ERROR));
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse NoResourceFoundException(NoResourceFoundException exception){
-        log.error("Resource Exception : ", exception);
+    public ErrorResponse NoResourceFoundException(NoResourceFoundException exception, HttpServletRequest request) {
+        Sentry.captureException(exception);
+        sendNotification(exception, request);
+
         return ErrorResponse.of(new CustomException(ErrorCode.RESOURCE_NOT_FOUND));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class, MissingServletRequestParameterException.class, HttpMessageNotReadableException.class})
-    public ErrorResponse handleBadRequestException(Exception exception) {
-        log.error("잘못된 요청 ", exception);
+    public ErrorResponse handleBadRequestException(Exception exception, HttpServletRequest request) {
+        Sentry.captureException(exception);
+        sendNotification(exception, request);
+
         return ErrorResponse.of(new CustomException(ErrorCode.BAD_REQUEST));
     }
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(CustomException exception) {
-        log.error("CustomException: ", exception);
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException exception, HttpServletRequest request) {
         Sentry.captureException(exception);
+        sendNotification(exception, request);
+
         return ResponseEntity.status(exception.getErrorCode().getStatus())
                 .body(ErrorResponse.of(exception));
+    }
+
+    private void sendNotification(Exception e, HttpServletRequest request) {
+        log.error("", e);
+        notificationService.sendNotification(e, request.getRequestURI(), getParams(request));
+    }
+
+    private String getParams(HttpServletRequest req) {
+        StringBuilder params = new StringBuilder();
+        Enumeration<String> keys = req.getParameterNames();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            params.append("- ").append(key).append(" : ").append(req.getParameter(key)).append("\n");
+        }
+        return params.toString();
     }
 }
