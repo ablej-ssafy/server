60806bd12f98ac5385d689c09a252ec24044feda
김용수 <akys159357@naver.com>
2024-11-01 16:31:11 +0900

    [#61]chore: 주석 제거

--- src/test/java/me/noteme/headhunting/domain/resume/controller/ResumeControllerTest.java
+++ src/test/java/me/noteme/headhunting/domain/resume/controller/ResumeControllerTest.java
@@ -4,7 +4,6 @@ import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import me.noteme.headhunting.common.filter.JWTFilter;
 import me.noteme.headhunting.core.annotation.CustomMockUser;
 import me.noteme.headhunting.core.support.RestDocsSupport;
-import me.noteme.headhunting.domain.member.controller.AuthController;
 import me.noteme.headhunting.domain.resume.controller.request.ResumeBasicRequest;
 import me.noteme.headhunting.domain.resume.dto.ResumeBasicResponse;
 import me.noteme.headhunting.domain.resume.dto.ResumePdfResponse;
