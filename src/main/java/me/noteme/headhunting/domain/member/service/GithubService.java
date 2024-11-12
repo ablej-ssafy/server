1f9ef3002b3f77dd9118f279e2ef069086826189
Kimbumsoo99 <show7441@naver.com>
2024-11-13 00:52:08 +0900

    [#104]feat: Github 분석 이메일 전송 API

--- src/main/java/me/noteme/headhunting/domain/member/service/GithubService.java
+++ src/main/java/me/noteme/headhunting/domain/member/service/GithubService.java
@@ -2,10 +2,16 @@ package me.noteme.headhunting.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.noteme.headhunting.common.exception.CustomException;
+import me.noteme.headhunting.common.exception.ErrorCode;
+import me.noteme.headhunting.common.listener.event.GithubAnalysisEvent;
 import me.noteme.headhunting.common.utils.KeyUtils;
+import me.noteme.headhunting.domain.member.entity.Member;
 import me.noteme.headhunting.domain.member.feign.GithubRequestClient;
 import me.noteme.headhunting.domain.member.feign.request.RepoInfoRequest;
 import me.noteme.headhunting.domain.member.feign.response.RepoAnalysisResponse;
+import me.noteme.headhunting.domain.member.repository.MemberRepository;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -13,15 +19,25 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GithubService {
     private final GithubRequestClient githubRequestClient;
+    private final ApplicationEventPublisher publisher;
+    private final MemberRepository memberRepository;
 
-    public RepoAnalysisResponse repoAnalysis(String owner, String repo, String branch, String token) {
+    public RepoAnalysisResponse repoAnalysis(Long memberId, String owner, String repo, String branch, String token) {
+        String email = memberRepository.findFetchById(memberId)
+                .map(Member::getUsername)
+                .orElseThrow(() -> new CustomException(ErrorCode.RESOURCE_NOT_FOUND));
+
         String requestId = KeyUtils.generateKey();
         return githubRequestClient.repoAnalysis(
-                RepoInfoRequest.of(requestId, owner, repo, branch, token)
+                RepoInfoRequest.of(requestId, owner, repo, branch, token, email)
         );
     }
 
     public RepoAnalysisResponse getRepoAnalysisStatus(String requestId) {
         return githubRequestClient.repoStatus(requestId);
+    }
+
+    public void sendAnalysisResult(String email, String repositoryName, String analysisSummary) {
+        publisher.publishEvent(GithubAnalysisEvent.of(email, repositoryName, analysisSummary));
     }
 }
