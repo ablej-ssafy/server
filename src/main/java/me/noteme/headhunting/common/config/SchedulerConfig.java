a086137da175af05968c8f83911673316ecb7bc2
김용수 <akys159357@naver.com>
2024-11-13 17:58:39 +0900

    [#102]feat: 스케줄러 적용

---
+++ src/main/java/me/noteme/headhunting/common/config/SchedulerConfig.java
@@ -0,0 +1,10 @@
+package me.noteme.headhunting.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+    // 추가 설정
+}
