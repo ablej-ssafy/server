79ab2509b168be8542a2ee915158ab48eae9d277
김용수 <akys159357@naver.com>
2024-11-14 09:57:58 +0900

    [#102]feat: 스프링 스케줄러 스레드 설정

--- src/main/java/me/noteme/headhunting/common/config/SchedulerConfig.java
+++ src/main/java/me/noteme/headhunting/common/config/SchedulerConfig.java
@@ -2,9 +2,23 @@ package me.noteme.headhunting.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
 @EnableScheduling
-public class SchedulerConfig {
-    // 추가 설정
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+        threadPoolTaskScheduler.setPoolSize(5); // 스레드 풀 사이즈 설정
+        threadPoolTaskScheduler.setThreadNamePrefix("scheduled-task"); // 스레드 이름 접두사
+        threadPoolTaskScheduler.setWaitForTasksToCompleteOnShutdown(true); // 종료 전 작업 완료 대기
+        threadPoolTaskScheduler.initialize();
+
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
 }
