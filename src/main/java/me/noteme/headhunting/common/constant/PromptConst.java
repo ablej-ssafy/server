dd70580d5c09100b9235f229ff4944d17ec5c11c
sunsuking <sunsuking@gmail.com>
2024-11-17 15:14:19 +0900

    hotfix: 프롬프트 변경

--- src/main/java/me/noteme/headhunting/common/constant/PromptConst.java
+++ src/main/java/me/noteme/headhunting/common/constant/PromptConst.java
@@ -54,16 +54,15 @@ public interface PromptConst {
                        - title should contain the name of the project or experience (e.g., "딸깍", "친환경 커스터마이징 쇼핑몰").
                        - affiliation should contain the role or position of the individual (e.g., "백엔드", "프론트엔드", "프로젝트 리더", "개발자").
                     3. Use the "YYYY-MM-DD" format for all dates that correspond to LocalDate. If the date is missing or listed as "진행중", replace it with today's date with the "YYYY-MM-DD" format.
-                    4. Omit optional fields like `referenceUrl` or `grade` if no values are available for them.
-                    5. In the description field:
+                    4. In the description field:
                        - Use proper punctuation, but ensure there is no trailing comma at the end of each sentence.
                        - Avoid adding unnecessary characters or symbols.
                        - Please write everything as it is without adding or summarizing anything new
                        - Please don't miss any content from the resume
                        - Description should contain writer's project or experience content (e.g., "역할", "기술스택", "개발 내용", "프로젝트 내용", "성과", "개발사항")
                        - If description has content's title, please write the content's title in the right placer
-                    6. Please put the '\\n' in order to fit each context.
-                    7. Please write the words in the right place so that you don't miss the content
+                    5. Please put the '\\n' in order to fit each context.
+                    6. Please write the words in the right place so that you don't miss the content
             """;
 
     String questionPrompt = """
